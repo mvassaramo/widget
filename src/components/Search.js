5410caf85ba7b4acc5ef0a17e7688f82c8f229a2
Maduri Vassaramo <maduri@vinterior.co>
2021-05-14 10:45:30 +0200

    add get request to Search component

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,7 +1,28 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 const Search = () => {
   const [query, setQuery] = useState('')
+  const [results, setResults] = useState([])
+
+  useEffect(() => {
+    const searchWiki = async () => {
+      const { data } = await(axios.get('https://en.wikipedia.org/w/api.php', {
+        params: {
+          action: 'query',
+          list: 'search',
+          origin: '*',
+          format: 'json',
+          srsearch: query
+        }
+      }))
+
+      setResults(data.query.search)
+    }
+      if (query) {
+        searchWiki()
+      }
+  }, [query])
 
   return(
     <div>
