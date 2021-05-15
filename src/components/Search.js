5a5278dbb901c562f48934b685eb3aa30095a96c
Maduri Vassaramo <maduri@vinterior.co>
2021-05-15 12:36:40 +0200

    add button to link to wiki page

--- src/components/Search.js
+++ src/components/Search.js
@@ -27,6 +27,11 @@ const Search = () => {
   const renderedResults = results.map(result => {
     return (
       <div className="item" key={result.pageid}>
+        <div className="right loaded content">
+          <a className="ui button" target="_blank" href={`https://en.wikipedia.org?curid=${result.pageid}`}>
+            Go
+          </a>
+        </div>
         <div className="content">
           <div className="header">{result.title}</div>
           <span dangerouslySetInnerHTML={{ __html: result.snippet}}></span>
