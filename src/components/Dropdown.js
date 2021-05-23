6d9b5587313c5b0c36ca41004b48a6e18bd325f9
Maduri Vassaramo <maduri@vinterior.co>
2021-05-23 08:50:47 +0200

    add funtionality to open/close dropdown

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -1,6 +1,8 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const DropDown = ({ options, selected, onSelectedChange }) => {
+  const [open, setOpen] = useState(false)
+
   const renderedOptions = options.map(option => {
 
     if (option.value === selected.value) return
@@ -15,14 +17,16 @@ const DropDown = ({ options, selected, onSelectedChange }) => {
       </div>
     )
   })
+
+  
   return(
     <div className="ui form">
       <div className="field">
         <label className="label">Select a colour:</label>
-        <div className="ui selection dropdown visible active">
+        <div onClick={() => setOpen(!open)} className={`ui selection dropdown ${open ? 'visible active' : ''}`}>
           <i className="dropdown icon"></i>
           <div className="text">{selected.label}</div>
-          <div className="menu visible transition">
+          <div className={`menu ${open ? 'visible transition' : ''}`}>
             {renderedOptions}
           </div>
         </div>
