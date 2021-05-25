3a7853f7f3b9d3ea3149900212a4c48031366564
Maduri Vassaramo <maduri@vinterior.co>
2021-05-25 09:59:17 +0200

    remove eventListener when Dropdown component not rendered

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -5,11 +5,17 @@ const DropDown = ({ options, selected, onSelectedChange }) => {
   const ref = useRef()
 
   useEffect(() => {
-    document.body.addEventListener('click', (event) => {
+    const onBodyClick = (event) => {
       if (ref.current.contains(event.target)) return;
 
       setOpen(false)
-    }, { capture: true })
+    }
+
+    document.body.addEventListener('click', onBodyClick, { capture: true })
+
+    return () => {
+      document.body.removeEventListener('click', onBodyClick, { capture: true })
+    }
   }, [])
 
   const renderedOptions = options.map(option => {
